704ecb19a36eaed15c7726e6a91085b7d76d381f
adityamishra2025 <adityamishra0506@gmail.com>
2025-10-11 23:04:33 +0530

    Remmoved Unnecessary comments

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Contact from './pages/Contact';
 import Policy from './pages/Policy';
 import Pagenotfound from './pages/Pagenotfound';
 import Register from './pages/Auth/Register';
-//  import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/Auth/Login';
 import Dashboard from './pages/user/Dashboard';
